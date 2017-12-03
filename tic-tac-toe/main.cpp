203d0ccb7de56ae34f6f8fb96c152fb8208c6b20
filip <filip.kozjak@gmail.com>
2017-12-03 10:38:25 +0100

    Initial Commit

---
+++ tic-tac-toe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tic-tac-toe
+//
+//  Created by Filip on 03/12/2017.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
